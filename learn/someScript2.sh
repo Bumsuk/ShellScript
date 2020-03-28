8bd51d5a62efa78063063fe7c4081396001acdf1
brown <xenix72@gmail.com>
2020-03-28 20:54:51 +0900

    some file added

---
+++ learn/someScript2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+set -u
+set -o pipefail
+
+echo "check!🤡"
---
+++ learn/test_0_variable.sh
@@ -0,0 +1,9 @@
+# set 명령으로 로컬로 선언된 변수들 정보 확인가능
+set
+
+# 로컬 변수는 당연하게도 열린 쉘에서만 접근가능
+ppengsu="펭수 안에 사람있다!🐧"
+echo $ppengsu
+unset ppengsu
+
+
