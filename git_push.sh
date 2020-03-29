0634a72efaa71bb3bc8f4d96ad3e222df4b88d07
brown <xenix72@gmail.com>
2020-03-30 00:28:23 +0900

    git_push.sh 업데이트(좀더 편하게)

--- git_push.sh
+++ git_push.sh
@@ -1,5 +1,15 @@
 # 암 생각없이 remote repo에 반영하고 싶을때 사용
 
+defaultMsg="update files"
+
+if [ $# = 0 ]; then
+  echo "커밋 커멘트 없이 진행합니다."
+else
+  defaultMsg=$1
+fi
+
+echo "확정된 msg : $defaultMsg"
+
 git add .
-git commit -m "[bump] update files"
+git commit -m "$defaultMsg"
 git push
