494a825fca5f81601178d1db7fb96edac39d0a9a
brown <xenix72@gmail.com>
2020-04-01 21:06:17 +0900

    git_push.sh update

--- git_push.sh
+++ git_push.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 # 쉽게 remote repo에 반영하고 싶을때 사용
 
+GREEN='\033[0;32m'
+RED='\033[1;31m'
+NC='\033[0m'
+
 defaultMsg="update files"
 
 if [ $# = 0 ]; then
-  echo "➡️커밋 커멘트를 입력하세요."
+  echo "${GREEN}➡️ 커밋 커멘트를 입력하세요.${NC}"
   read comment
   defaultMsg=$comment
 else
@@ -14,10 +18,10 @@ fi
 E_BADARGS=65
 
 if [ ! -n "$defaultMsg" ]; then
-	echo "🚫명령을 제대로 입력하세요. 공백은 허용안됨!"; exit
+	echo "${RED}🚫 명령을 제대로 입력하세요. 공백은 허용안됨!${NC}"; exit
 fi
 
-echo "확정된 msg : $defaultMsg"
+echo "$확정된 msg : $defaultMsg"
 
 function process() {
   git add .
