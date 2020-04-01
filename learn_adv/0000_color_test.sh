08583486dcee261400b37eee8a7384e1a6434a3b
brown <xenix72@gmail.com>
2020-04-01 20:59:15 +0900

    출력 샘플 추가

---
+++ learn_adv/0000_color_test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# 색상정보 변수들은 타 파일에서 가져옴.
+source ./0000_color.sh
+
+echo -e "${DARKGRAY}다크그레이!"
+echo -e "${RED}빨간색!"
+echo -e "${LIGHTRED}볼드 빨간색!"
+echo -e "${GREEN}그린색!!"
+echo -e "${YELLOW}노란색!"
+echo -e "${BLUE}파란색!"
+echo -e "${PURPLE}보라색!"
+echo -e "${LIGHTPURPLE}볼드 보라색!"
+echo -e "${CYAN}청록색"
+
+# echo "${RED_YELLOW} 경고! 이건 경고입니다!\033[0;0m"
+echo -e "${RED} 🚫 경고! 이건 경고입니다!"
+echo -e "\033[40;37m Hello World\033[0m"
+echo -e "\033[40;37;7m Hello World\033[0m"
+
+echo -e "\033[33;44mYellow text on blue background\033[0m"
+echo -e "\033[1;33;44mBold yellow text on blue background\033[0m"
+echo -e "\033[1;4;33;44mBold yellow underlined text on blue background\033[0m"
+
+echo -e "\033[43;31mSoftware is good!\033[0m"
+echo -e "\033[43;31m나랏말싸미\033[0m"
+
+# 이걸로 하얀색으로 원복!
+echo -e "\033[0m"
+
+
