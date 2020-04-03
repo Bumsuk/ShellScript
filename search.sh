373d109953ebf96b0ddece0f0d46c16131f54d7a
brown <xenix72@gmail.com>
2020-04-03 17:01:35 +0900

    change update

---
+++ search.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ ! -n $1 ]]; then
+  echo "🤡 파일명을 꼭 입력하세요!"; exit 1;
+fi
+
+search=$1
+
+# 현재 폴더의 스크립트 파일들 중에서, 07로 시작하는 파일들 검색 및 출력!
+results=()
+for file in *.sh; do
+  echo "[list] $file"
+  if [[ $file == ${search}* ]]; then
+    echo "🤡 ${file}"
+    results=("${results[@]}" $file)
+  fi
+done
+
+echo "결과 : results count : ${#results[@]}"
+echo "results : ${results[@]}"
